cfdc3efe05d83c101097c72c43cabd59bd303da9
Alyssa Ayala <alyssa.ayala29@qmail.cuny.edu>
2024-01-06 18:46:49 -0500

    Update PlexFileMover.jar

--- src/PlexFileMover.java
+++ src/PlexFileMover.java
@@ -1,11 +1,10 @@
 import java.io.File;
-import java.util.*;
 
 public class PlexFileMover {
 	public static void main(String[] args) {
 
+		// Get the current working directory
 		File dir = new File(System.getProperty("user.dir"));
-
 		if (dir.isDirectory()) {
 			start(dir.listFiles(), dir.getPath());
 		}
@@ -14,10 +13,13 @@ public class PlexFileMover {
 	public static void start(File[] files, String dir) {
 		for (File file: files) {
 			if (!file.isDirectory() && !file.getName().equals("PlexFileMover.jar")) {
+				
+				// Get file paths and names without extensions
 				File f = new File(dir + "/" + file.getName());
 				String path = f.getPath().replaceFirst("[.][^.]+$", "");
 				String name = f.getName().replaceFirst("[.][^.]+$", "");
 				
+				// Make directories and move files
 				File moved = new File(path);
 				moved.mkdir();
 				f.renameTo(new File (moved.getPath() + "/" + file.getName()));
