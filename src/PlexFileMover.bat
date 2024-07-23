b3d434a83569d1902b6b0dacce1407c047a402bc
Alyssa Ayala <alyssa.ayala29@qmail.cuny.edu>
2024-07-23 17:15:58 -0400

    Update PlexFileMover.bat

--- src/PlexFileMover.bat
+++ src/PlexFileMover.bat
@@ -3,7 +3,7 @@ title PlexFileMover.bat
 for %%f in (*.*) do (
 	if NOT "%%f" == "PlexFileMover.bat" (
 		
-		REM Create directory if it doesn't exist, and more corresponding file to it
+		REM Create directory if it doesn't exist, and more corresponding files to it
 		if NOT exist "%cd%/%%~nf" mkdir "%cd%/%%~nf"
 		move %%f "%cd%/%%~nf"
 		echo %%~nf
